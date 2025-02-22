e29b464f7e0de0f2dd35e39ebe944a5c08a411d9
Eduardo Matos <eduardomts144@gmail.com>
2025-02-21 23:06:33 -0300

    feat: adiciona frontEnd no cadastro de usuário

---
+++ src/pages/admin/register/index.js
@@ -0,0 +1,149 @@
+import { use, useState } from "react";
+
+import Cookies from 'js-cookie';
+import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+
+import AuthBox from "../../../components/auth-box";
+import Button from "../../../components/button";
+import Input from "../../../components/input";
+
+import { Container } from "./styled";
+
+// import AuthApi from "../../../service/authApi";
+// const api = new AuthApi();
+
+export default function Register() {
+    const [name, setName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [group, setGroup] = useState("");
+
+    const options = ["Admin", "Estoquista"];
+
+    const navigation = useNavigate();
+
+    const isFormCompleted = Object.values(
+        [email, password, confirmPassword, name, cpf, group]
+    ).every((value) => value.trim() !== "");
+
+    const isInvalidEmail = () => {
+        const emailRegex = /^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+        return !emailRegex.test(email);
+    }
+
+    const register = async () => {
+        if (!isFormCompleted) {
+            toast.warn("Preencha todos os campos !")
+            return;
+        }
+
+        if (password != confirmPassword) {
+            toast.warn("As senhas não são iguais !")
+            return;
+        }
+
+        if (isInvalidEmail()) {
+            toast.warn("O email deve ser bem formatado");
+            return;
+        }
+    }
+
+    return (
+        <Container>
+            <Link to={"/"}>
+                <img src="/assets/images/icon_logo.png" alt="logo-image" />
+            </Link>
+            <AuthBox myWidth={40} myHeight={70}>
+                <h3> Acesse sua conta </h3>
+                <hr />
+                <div>
+                    <Input
+                        myGetter={name}
+                        mySetter={setName}
+                        myMargin={"1.5em 0em"}
+                        myHeight={7}
+                        myWidth={28}
+                        myPlaceHolder="João Silva"
+                    >
+                        Seu Nome
+                    </Input>
+
+                    <Input
+                        myGetter={cpf}
+                        mySetter={setCpf}
+                        myMargin={"1.5em 0em"}
+                        myHeight={7}
+                        myWidth={28}
+                        myPlaceHolder="999.999.99-99"
+                    >
+                        Seu CPF
+                    </Input>
+
+                    <Input
+                        myGetter={email}
+                        mySetter={setEmail}
+                        myMargin={"1.5em 0em"}
+                        myHeight={7}
+                        myWidth={28}
+                        myPlaceHolder="email@email.com"
+                    >
+                        Sua Email
+                    </Input>
+
+                    <Input
+                        myGetter={password}
+                        mySetter={setPassword}
+                        myMargin={"1.5em 0em"}
+                        myHeight={7}
+                        myWidth={28}
+                        myType="password"
+                        myPlaceHolder="*****"
+                    >
+                        Sua senha
+                    </Input>
+
+                    <Input
+                        myGetter={confirmPassword}
+                        mySetter={setConfirmPassword}
+                        myMargin={"1.5em 0em"}
+                        myHeight={7}
+                        myWidth={28}
+                        myType="password"
+                        myPlaceHolder="*****"
+                    >
+                        Confirme sua Senha
+                    </Input>
+
+                    <div>
+                        <label htmlFor="select">Escolha uma opção:</label>
+                        <select
+                            id="select"
+                            value={group}
+                            onChange={(e) => setGroup(e.target.value)}
+                        >
+                            <option value="">Selecione...</option>
+                            {options.map((option, index) => (
+                                <option key={index} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <Button
+                        myMargin={"1.5em 0em"}
+                        myHeight={5}
+                        myWidth={28.6}
+                        myBackgroundColor={"yellow"}
+                        myMethod={register}
+                    >
+                        Entrar
+                    </Button>
+                </div>
+            </AuthBox>
+        </Container>
+    )
+}
